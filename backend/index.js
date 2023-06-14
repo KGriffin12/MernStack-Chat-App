0805ffbfb8d85ff359d717341b9e49a957048eab
Karlie Griffin <113546703+KGriffin12@users.noreply.github.com>
2023-06-13 22:42:38 -0500

    Cant get request.rest to populate still getting errors but saving progress.

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,19 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
-  return res.json({ username: username, secret: "sha256..." });
+
+  try {
+    const r = await axios.put(
+        "https://api.chatengine.io/users/",
+        { username: username, secret: username, first_name: username },
+        { headers: { "private-key": "dceefb80-e9b6-4de7-a8ad-0648523baccf" } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
 });
 
-app.listen(3000);
+app.listen(3001);
+//dceefb80-e9b6-4de7-a8ad-0648523baccf
+//return res.status(e.response.status).json(e.response.data);
